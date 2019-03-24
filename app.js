c715f7da6a6bf617109b3cf0938bd5776beebd20
ai-chen2050 <1033467071@qq.com>
2019-03-24 23:40:51 +0800

    add save QRimage

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 App({
-  onLaunch: function () {
+  
+    // 小程序初始化完成后，会全局触发一次
+    onLaunch: function () {
+        var that = this;
+        wx.getSetting({
+            success(res) {
+              if (!res.authSetting['scope.writePhotosAlbum']) {
+                wx.authorize({
+                  scope: 'scope.writePhotosAlbum',
+                  success() {
+                    console.log('scope.weitePhotosAlbum is success')
+                    
+                }
+                })
+              }
+            }, 
+            fail: function (res) {
+                console.log('scope.weitePhotosAlbum is fail')
+          }
+        })
+    },
+    
 
-  }
+    // 从后台到前台，小程序页面在手机当前窗口显示，会调用
+    onShow: function(options) {
+        let option = JSON.stringify(options);
+        this.globalData.scene=options.scene;
+        console.log('app.js option-----' + option)
+        console.log('app.js--------------------' + options.scene);
+    },
+
+    // 从前台到后台，当按了返回键，或 home 键，小程序进入后台，会调用
+    onHide: function() {
+
+    },
+
+    globalData: {
+        userInfo: null,
+        scene:""
+      }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,25 @@
 const app = getApp()
 
+const htmlSnip =
+`<div class="div_class">
+  <h1>Title</h1>
+  <p class="p">
+    Life is&nbsp;<i>like</i>&nbsp;a box of
+    <b>&nbsp;chocolates</b>.
+  </p>
+</div>
+`
+const weChatIndex = 
+`<><a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg4NjExNjM4OQ==&scene=124#wechat_redirect" target="_blank">https://mp.weixin.blakeFire</a>
+`
+
 Page({
   data: {
-
+      htmlSnip,
+      weChatIndex,
+      scenes_value: app.globalData.scene
   },
+
   onLoad: function () {
     console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
     console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
